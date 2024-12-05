dacda2cff80b0162f040b2197c0b0e19956c9f32
Mouta <mo_mg99@hotmail.com>
2024-12-05 13:59:33 +0200

    Update the context_usage_example

--- context_usage_example.py
+++ context_usage_example.py
@@ -15,29 +15,29 @@ max_assignments: dict[int, int] = {
 
 assignments: list[int | None] = []
 
-context: Context = {}
+context: Context = {"max_assignments": max_assignments}
 
 
 while remaining_tasks:
     available_robots = manage_robot_tasks(
         [assignments[-1]] if assignments else [],
-        max_assignments,
+        {},
         context=context,
     )
 
-    # If any robot is available, assign the task to it, else make a blank assignment
     if available_robots:
         assignments.append(available_robots[0])
         remaining_tasks -= 1
-    else:
+    elif context["max_assignments"]:  # Robots can still take tasks
         assignments.append(None)
-        # Break if there are (cooldown + 1) blank assignments, as robots can no longer take tasks.
-        if len(assignments) > 5 and all(
-            assignment is None for assignment in assignments[-6:]
-        ):
-            break
+    else:
+        break
 
-print(
-    f"Assignments: {assignments if not remaining_tasks else assignments[:-6]}"
-)
-print(f"Number of remaining tasks: {remaining_tasks}")
+
+print("=======")
+print(f"Assignments: {assignments}")
+print("-----")
+print(f"Remaining Tasks: {remaining_tasks}")
+print("-----")
+print(f"Final Context: {context}")
+print("=======")
